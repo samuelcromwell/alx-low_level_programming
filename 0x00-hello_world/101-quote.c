ebec4ef16e753d9108ba31d75a0ea7bf81e0fe0c
samuelcromwell <cromwellsamuel3@gmail.com>
2023-02-16 14:14:58 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+
+
 /**
  * main - Prints a statement to the standard error
  *
@@ -7,6 +9,6 @@
  */
 int main(void)
 {
-        fprintf(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+        fprintf(stderr, "\"And that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
         return (1);
 }
