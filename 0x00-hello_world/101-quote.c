bded4ac628ce725cef4202e03e6d061b1b1ae85e
samuelcromwell <cromwellsamuel3@gmail.com>
2023-02-16 14:30:17 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - Prints a statement to the standard error
- *
- * Return: 1
+
+/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ *followed by a new line, to standard error.
+ * Return: Always 1.
  */
+
 int main(void)
 {
-        fprintf(stderr," \ and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+        write(2, " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
         return (1);
 }
