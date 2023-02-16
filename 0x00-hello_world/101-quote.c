b0a4bf503fae87f53aca1026be9a5fa6fde7fe90
samuelcromwell <cromwellsamuel3@gmail.com>
2023-02-16 14:44:45 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
 /**
- *main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
- *followed by a new line, to standard error.
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to standard error.
  * Return: Always 1.
  */
 
