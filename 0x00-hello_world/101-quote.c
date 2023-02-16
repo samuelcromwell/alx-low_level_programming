ef8ad6bc56620e01ac8703f96f82086c7fc920ef
samuelcromwell <cromwellsamuel3@gmail.com>
2023-02-16 14:01:54 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
+
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ * main - Prints a statement to the standard error
+ *
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return(1);
+        fprintf(stderr, "\"And that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+        return (1);
 }
