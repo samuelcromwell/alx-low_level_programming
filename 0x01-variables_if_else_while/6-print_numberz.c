131fb81b2380de5e63ab80e1bef996d369cedd10
samuelcromwell <cromwellsamuel3@gmail.com>
2023-02-17 14:11:44 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,9 @@
  */
 int main(void)
 {
- 	int num;
-
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-	
-	putchar('\n');
-
-	return (0);
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
 }
