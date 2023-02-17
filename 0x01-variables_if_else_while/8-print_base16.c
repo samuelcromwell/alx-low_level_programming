e109abe2d69f1b18901daa105cfc0dd16339ede1
samuelcromwell <cromwellsamuel3@gmail.com>
2023-02-17 14:16:33 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,13 @@
  */
 int main(void)
 {
-	int num;
-	char la;
-
-	for (num = 0; num < 10; num++)
-		putchar ((num % 10) + '0');
-
-	for (la = 'a'; laa <= 'f'; la++)
-		putchar(la);
-
-	putchar('\n');
-
-	return (0);
+int num;
+char la;
+for (num = 0; num < 10; num++)
+putchar ((num % 10) + '0');
+for (la = 'a'; laa <= 'f'; la++)
+putchar(la);
+putchar('\n');
+return (0);
 }
+
