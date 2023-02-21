215be755039f3f99dcc5e6c7ca3f8493d30cbee9
samuelcromwell <cromwellsamuel3@gmail.com>
2023-02-21 14:30:57 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10	- print 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
 
 void print_alphabet_x10(void)
