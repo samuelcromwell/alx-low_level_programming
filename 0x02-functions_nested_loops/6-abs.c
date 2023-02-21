d92df14f80c434ec4165587dadafb18bc159bc7a
samuelcromwell <cromwellsamuel3@gmail.com>
2023-02-21 15:17:54 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - gives absolute values
+ * @n: The parameters
+ * Return: abs of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
