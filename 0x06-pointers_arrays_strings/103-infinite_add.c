9e37cb76d8948a739d596cbe08c2b6d0939580c9
samuelcromwell <cromwellsamuel3@gmail.com>
2023-03-02 12:30:29 -0800

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,55 +1,75 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * infinite_add - Adds two numbers
- * @n1: The first number
- * @n2: The second number
- * @r: The buffer to store the result
- * @size_r: the buffer size
- * Return:  A pointer to the result
- */
+* rev_string - reverse array
+* @n: integer params
+* Return: 0
+*/
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void rev_string(char *n)
 {
-	int d1 = 0, d2 = 0, pq, ch, es1, es2, add = 0;
+int i = 0;
+int j = 0;
+char temp;
+while (*(n + i) != '\0')
+{
+i++;
+}
+i--;
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
+}
 
-	while (*(n1 + d1) != '\0')
-		d1++;
-	while (*(n2 + d2) != '\0')
-		d2++;
-	if (d1 >= d2)
-		ch = d1;
-	else
-		ch = d2;
-	if (size_r <= ch + 1)
-		return (0);
-	r[ch + 1] = '\0';
-	d1--, d2--, size_r--;
-	es1 = *(n1 + d1) - 48, es2 = *(n2 + d2) - 48;
-	while (ch >= 0)
-	{
-		pq = es1 + es2 + add;
-		if (pq >= 10)
-			add = pq / 10;
-		else
-			add = 0;
-		if (pq > 0)
-		*(r + ch) = (pq % 10) + 48;
-		else
-			*(r + ch) = '0';
-		if (d1 > 0)
-			d1--, es1 = *(n1 + d1) - 48;
-		else
-			es1 = 0;
-		if (d2 > 0)
-			d2--, es2 = *(n2 + d2) - 48;
-		else
-			es2 = 0;
-		ch--, size_r--;
-	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+/**
+* infinite_add - add 2 numbers together
+* @n1: text representation of 1st number to add
+* @n2: text representation of 2nd number to add
+* @r: pointer to buffer
+* @size_r: buffer size
+* Return: pointer to calling function
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int overflow = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
+while (*(n1 + i) != '\0')
+i++;
+while (*(n2 + j) != '\0')
+j++;
+i--;
+j--;
+if (j >= size_r || i >= size_r)
+return (0);
+while (j >= 0 || i >= 0 || overflow == 1)
+{
+if (i < 0)
+val1 = 0;
+else
+val1 = *(n1 + i) - '0';
+if (j < 0)
+val2 = 0;
+else
+val2 = *(n2 + j) - '0';
+temp_tot = val1 + val2 + overflow;
+if (temp_tot >= 10)
+overflow = 1;
+else
+overflow = 0;
+if (digits >= (size_r - 1))
+return (0);
+*(r + digits) = (temp_tot % 10) + '0';
+digits++;
+j--;
+i--;
+}
+if (digits == size_r)
+return (0);
+*(r + digits) = '\0';
+rev_string(r);
+return (r);
 }
