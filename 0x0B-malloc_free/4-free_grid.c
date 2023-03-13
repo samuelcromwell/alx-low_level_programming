be5b932b6f7a00b770f651bbefe9c7de03f62580
samuelcromwell <cromwellsamuel3@gmail.com>
2023-03-13 13:37:24 -0700

    Task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * *free_grid - frees a 2 dimensional grid previously created 
- * @grid: grid to free
+ * *free_grid - frees a 2 dimensional grid previously created
+ * * @grid: grid to free
  * @height: height of the grid
  *
  * Return: nothing
