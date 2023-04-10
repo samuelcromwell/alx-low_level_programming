81926da9486fbb370e64f37a1f11766a68baca65
samuelcromwell <cromwellsamuel3@gmail.com>
2023-04-10 10:55:49 -0700

    Print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,10 +7,21 @@
 
 void print_binary(unsigned long int n)
 {
+int i, count = 0;
+unsigned long int current;
 
-if
-(n > 1);
-print_binary(n >> 1);
+for (i = 63; i >= 0; i--)
+{
+current = n >> i;
 
-_putchar((n & 1) + '0');
+if (current & 1)
+{
+_putchar('1');
+count++
+}
+else if (count)
+_putchar('0');
+}
+if (!count)
+_putchar('0')
 }
